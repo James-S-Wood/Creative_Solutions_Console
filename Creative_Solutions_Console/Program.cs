34f5e3c76d76b51737e7aa635df2fb2286cfea83
jameswood <james_s_wood@outlook.com>
2021-03-05 13:49:53 -0500

    Initial commit

---
+++ Creative_Solutions_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Creative_Solutions_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I amd looking forward to learning C# and the .Net Framework.");
+            
+        }
+    }
+}

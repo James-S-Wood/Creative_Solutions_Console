783f1a909de2752249d2f06d0fed3f108d8d3af3
jameswood <james_s_wood@outlook.com>
2021-03-05 14:07:59 -0500

    Added another writeline to file

--- Creative_Solutions_Console/Program.cs
+++ Creative_Solutions_Console/Program.cs
@@ -7,8 +7,11 @@ namespace Creative_Solutions_Console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("I amd looking forward to learning C# and the .Net Framework.");
+            Console.WriteLine("I amd looking forward to learning C# and the " +
+                              ".Net Framework.");
             Console.WriteLine("Just going to add another line for fun.");
+            Console.WriteLine("Added another file to check for success in " +
+                              "plug-in being disabled ");
         }
     }
 }
